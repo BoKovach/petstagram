303f7d53b1a57bb908d747682d803f898f39babe
Bo Kovach <bo.kovachh@gmail.com>
2024-01-27 19:52:00 +0200

    Extracted DB config into evn variables

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from os.path import join
 from pathlib import Path
 
@@ -82,11 +83,11 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'petdb',
-        'USER': 'postgres',
-        'PASSWORD': '1930',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
