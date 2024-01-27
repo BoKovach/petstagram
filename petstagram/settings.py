6764994501a3e08b5b8e50004d445df2fee78f4f
Bo Kovach <bo.kovachh@gmail.com>
2024-01-27 20:33:40 +0200

    Extracted ALLOWED_HOSTS and Debug into env vars

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,13 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-52v!01p)c)o3i!0(gw#uju-_7^&4t38ke@-7jjq_m6o29bs@4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'petstagram-demo-80b11e283116.herokuapp.com',
-]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
